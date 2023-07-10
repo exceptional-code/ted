0111bfd7d70a068e13fc154db9b1c7af7ac2e4d5
bryceAM <bryce.malmberg@yahoo.com>
2023-07-09 23:34:28 -0700

    feat: create file architecture

---
+++ src/axios-services/index.js
@@ -0,0 +1,4 @@
+// this file will hold all the front-end network request adapter functions
+// modifies requests under given conditions
+// gives user input about the state of communication between front-end and api
+// the goal here is not to store .get, .put, .post, etc. requests like the api requests but to modify said requests under certain conditions
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import ReactDOM
+// import BrowserRouter
+// import App or Root or Server or whatever you want to call it
+// import the index.css file here for global css styles
+
+// define the root
+// root.render with browser router sandwiching the newly created app/root/server
