98ab1e4b2ad79b7c6d12e4efa874c634aff4ea5c
bryceAM <bryce.malmberg@yahoo.com>
2023-07-21 20:33:51 -0700

    fix: reorganize axios and api architecture

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+    This module creates the web server to be deployed so clients can connect
+    using a pooled connection system. The deployed server will be the middle man
+    between the client's app and the third party API/Database to make the most
+    efficient use of connections
+*/
