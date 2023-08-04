36d79531d61aa76f0b1ba544c014067036a7bba9
bryceAM <bryce.malmberg@yahoo.com>
2023-08-03 22:58:03 -0700

    feat: initialize App component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+
+    // render components in vDOM
+    return (
+        <div>hello world!</div>
+    )
+}
+
+export default App;
